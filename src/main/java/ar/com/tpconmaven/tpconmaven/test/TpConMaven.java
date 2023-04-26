204c6ae2fe7921922ef2066b1cc3a3d973ab6101
feedex20 <fedelazarte2015@gmail.com>
2023-04-26 17:43:30 -0300

    primer commit tp

---
+++ src/main/java/ar/com/tpconmaven/tpconmaven/test/TpConMaven.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ar.com.tpconmaven.tpconmaven.test;
+
+/**
+ *
+ * @author EXO
+ */
+public class TpConMaven {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
